570b4c21e0a5e731452c79d97c6779f9e0ca9312
Vlad Kirov <wladponomar@gmail.com>
2024-01-27 13:56:14 +0300

    feat: hibernate session factory

---
+++ src/main/java/org/example/util/hibernate/HibernateSessionFactory.java
@@ -0,0 +1,32 @@
+package org.example.util.hibernate;
+
+import org.example.model.Good;
+import org.example.model.Order;
+import org.example.model.Role;
+import org.example.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactory {
+    private static SessionFactory sessionFactory;
+
+    private HibernateSessionFactory(){}
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(User.class);
+                configuration.addAnnotatedClass(Role.class);
+                configuration.addAnnotatedClass(Good.class);
+                configuration.addAnnotatedClass(Order.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return sessionFactory;
+    }
+}
