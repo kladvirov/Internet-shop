5e64e4285edc42eb43449f1a4197a6a527ec285a
Vlad Kirov <wladponomar@gmail.com>
2024-01-28 21:01:54 +0300

    style: changed few lines

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,10 @@ package org.example;
 import org.example.model.Order;
 import org.example.model.Role;
 import org.example.model.User;
-import org.example.service.RoleService;
+import org.example.repository.hibernate.UserRepositoryImpl;
 import org.example.service.UserService;
+import org.example.util.hibernate.HibernateSessionFactory;
+import org.hibernate.SessionFactory;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
@@ -15,13 +17,14 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
+        SessionFactory sessionFactory = HibernateSessionFactory.getSessionFactory();
+        UserRepositoryImpl userRepository = new UserRepositoryImpl(sessionFactory);
+        UserService userService = new UserService(userRepository);
 
-        UserService userService = new UserService();
         Order order = new Order(new Timestamp(3432423), "ordered");
         Role role1 = new Role("alo");
         Role role2 = new Role("java");
         Set<Role> roles = new HashSet<>();
-        RoleService roleService = new RoleService();
         User user1 = new User("Testin", "HibernateRoles228", LocalDate.of(2004, 8, 6), "123", "56789", false);
         User user2 = new User("Hiber", "Testin", LocalDate.of(2004, 8, 6), "123", "56789", false);
 
@@ -31,6 +34,7 @@ public class Main {
         users.add(user2);
 
         role1.setUsers(users);
-        roleService.saveRole(role1);
+
+        userService.deleteUser(13L);
     }
 }
