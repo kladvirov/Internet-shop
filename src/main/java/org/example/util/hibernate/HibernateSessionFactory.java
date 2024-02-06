7c3dbafe0872dd1339cda11df057b1892b2c2d88
Vlad Kirov <wladponomar@gmail.com>
2024-02-06 11:10:05 +0300

    feat: add annotated classes

--- src/main/java/org/example/util/hibernate/HibernateSessionFactory.java
+++ src/main/java/org/example/util/hibernate/HibernateSessionFactory.java
@@ -1,5 +1,9 @@
 package org.example.util.hibernate;
 
+import org.example.model.Good;
+import org.example.model.Order;
+import org.example.model.Role;
+import org.example.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -13,6 +17,10 @@ public class HibernateSessionFactory {
         if (sessionFactory == null) {
             try {
                 Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(Role.class);
+                configuration.addAnnotatedClass(User.class);
+                configuration.addAnnotatedClass(Good.class);
+                configuration.addAnnotatedClass(Order.class);
                 StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
                 sessionFactory = configuration.buildSessionFactory(builder.build());
             } catch (Exception e){
