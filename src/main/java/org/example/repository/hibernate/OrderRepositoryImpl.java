a2454d184332bf0ee3204b8a804b980b35528cc5
Vlad Kirov <wladponomar@gmail.com>
2024-02-06 11:15:12 +0300

    feat: added rollbacks

--- src/main/java/org/example/repository/hibernate/OrderRepositoryImpl.java
+++ src/main/java/org/example/repository/hibernate/OrderRepositoryImpl.java
@@ -1,5 +1,6 @@
 package org.example.repository.hibernate;
 
+import org.example.exception.RepositoryException;
 import org.example.model.Order;
 import org.example.repository.OrderRepository;
 import org.hibernate.HibernateException;
@@ -13,7 +14,7 @@ public class OrderRepositoryImpl implements OrderRepository {
 
     private final SessionFactory sessionFactory;
 
-    OrderRepositoryImpl(SessionFactory sessionFactory) {
+    public OrderRepositoryImpl(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
 
@@ -21,8 +22,8 @@ public class OrderRepositoryImpl implements OrderRepository {
     public Order findById(Long id) {
         try (Session session = sessionFactory.openSession()) {
             return session.get(Order.class, id);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (HibernateException e) {
+            throw new RepositoryException("There was an exception during finding Order by id");
         }
     }
 
@@ -30,43 +31,52 @@ public class OrderRepositoryImpl implements OrderRepository {
     public List<Order> findAll() {
         try (Session session = sessionFactory.openSession()) {
             return session.createQuery("From Order", Order.class).list();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (HibernateException e) {
+            throw new RepositoryException("There was an exception during finding all Orders");
         }
     }
 
     @Override
     public Order save(Order order) {
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.save(order);
-            transaction.commit();
-        } catch (HibernateException e) {
-            throw new RuntimeException(e);
+            try {
+                Transaction transaction = session.beginTransaction();
+                session.save(order);
+                transaction.commit();
+            } catch (HibernateException e) {
+                session.getTransaction().rollback();
+                throw new RepositoryException("There was an exception during saving Good");
+            }
+            return order;
         }
-        return order;
     }
 
     @Override
     public void update(Order order) {
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.update(order);
-            transaction.commit();
-        } catch (HibernateException e) {
-            throw new RuntimeException(e);
+            try {
+                Transaction transaction = session.beginTransaction();
+                session.update(order);
+                transaction.commit();
+            } catch (HibernateException e) {
+                session.getTransaction().rollback();
+                throw new RepositoryException("There was an exception during updating Good");
+            }
         }
     }
 
     @Override
     public void delete(Long id) {
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            Order order = session.load(Order.class, id);
-            session.delete(order);
-            transaction.commit();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            try {
+                Transaction transaction = session.beginTransaction();
+                Order order = session.load(Order.class, id);
+                session.delete(order);
+                transaction.commit();
+            } catch (HibernateException e) {
+                session.getTransaction().rollback();
+                throw new RepositoryException("There was an exception during saving Good");
+            }
         }
     }
 }
