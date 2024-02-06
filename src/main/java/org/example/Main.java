ee62c1c37b854a211b487d794bae2c8a5668e903
Vlad Kirov <wladponomar@gmail.com>
2024-02-06 11:09:27 +0300

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,17 @@
 package org.example;
 
-import org.example.model.Order;
 import org.example.model.Role;
 import org.example.model.User;
+import org.example.repository.hibernate.RoleRepositoryImpl;
 import org.example.repository.hibernate.UserRepositoryImpl;
+import org.example.service.RoleService;
 import org.example.service.UserService;
 import org.example.util.hibernate.HibernateSessionFactory;
 import org.hibernate.SessionFactory;
 
-import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
-
 
 public class Main {
     public static void main(String[] args) {
@@ -21,20 +20,16 @@ public class Main {
         UserRepositoryImpl userRepository = new UserRepositoryImpl(sessionFactory);
         UserService userService = new UserService(userRepository);
 
-        Order order = new Order(new Timestamp(3432423).toLocalDateTime(), "ordered");
+        RoleRepositoryImpl roleRepository = new RoleRepositoryImpl(sessionFactory);
+        RoleService roleService = new RoleService(roleRepository);
+
+//        Order order = new Order(new Timestamp(3432423).toLocalDateTime(), "ordered");
         Role role1 = new Role("alo");
         Role role2 = new Role("java");
         Set<Role> roles = new HashSet<>();
-        User user1 = new User("Testin", "HibernateRoles228", LocalDate.of(2004, 8, 6), "123", "56789", false);
+        User user1 = new User("blabla", "HibernateRoles228", LocalDate.of(2004, 8, 6), "123", "56789", false);
         User user2 = new User("Hiber", "Testin", LocalDate.of(2004, 8, 6), "123", "56789", false);
 
         Set<User> users = new HashSet<>();
-
-        users.add(user1);
-        users.add(user2);
-
-        role1.setUsers(users);
-
-        userService.deleteUser(13L);
     }
 }
