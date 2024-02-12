ff3becd3524705197b876f9f712fb3b5ab515335
Vlad Kirov <wladponomar@gmail.com>
2024-02-12 12:26:47 +0300

    fix: changed fields

--- src/main/java/org/example/dto/OrderCreationDto.java
+++ src/main/java/org/example/dto/OrderCreationDto.java
@@ -1,15 +1,16 @@
 package org.example.dto;
 
-import lombok.*;
-
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.model.User;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderCreationDto {
 
-    private LocalDateTime orderDate;
+    private User user;
 
     private String status;
 }
--- src/main/java/org/example/dto/OrderDto.java
+++ src/main/java/org/example/dto/OrderDto.java
@@ -1,11 +1,20 @@
 package org.example.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.model.User;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDto {
+
+    private User user;
+
+    private LocalDateTime orderDate;
 
     private String status;
 }
