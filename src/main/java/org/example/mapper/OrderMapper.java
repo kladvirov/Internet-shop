c029b3dc2fea0c6969713213a7612f8add38ff21
Vlad Kirov <wladponomar@gmail.com>
2024-02-12 12:29:06 +0300

    feat: changed fields

--- src/main/java/org/example/mapper/OrderMapper.java
+++ src/main/java/org/example/mapper/OrderMapper.java
@@ -4,13 +4,15 @@ import org.example.dto.OrderCreationDto;
 import org.example.dto.OrderDto;
 import org.example.model.Order;
 
+import java.time.LocalDateTime;
+
 public class OrderMapper {
 
     public OrderDto toDto(Order order) {
-        return new OrderDto(order.getStatus());
+        return new OrderDto(order.getUser(), order.getOrderDate(), order.getStatus());
     }
 
     public Order toEntity(OrderCreationDto orderDto) {
-        return new Order(orderDto.getOrderDate(), orderDto.getStatus());
+        return new Order(LocalDateTime.now(), orderDto.getStatus(), orderDto.getUser());
     }
 }
