e6ceefd49b9f546bc37ed17003e50cc9c7322b3d
Vlad Kirov <wladponomar@gmail.com>
2024-02-27 20:40:41 +0300

    feat: try-catch added

--- src/main/java/by/kladvirov/service/GoodService.java
+++ src/main/java/by/kladvirov/service/GoodService.java
@@ -3,11 +3,13 @@ package by.kladvirov.service;
 import by.kladvirov.dto.GoodCreationDto;
 import by.kladvirov.dto.GoodDto;
 import by.kladvirov.dto.GoodUpdateDto;
+import by.kladvirov.exception.RepositoryException;
 import by.kladvirov.exception.ServiceException;
 import by.kladvirov.mapper.GoodMapper;
 import by.kladvirov.model.Good;
 import by.kladvirov.repository.GoodRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,25 +23,41 @@ public class GoodService {
     private final GoodMapper goodMapper;
 
     public GoodDto findById(Long id) {
-        Good good = goodRepository.findById(id).orElseThrow(() -> new ServiceException("There is no entity with the following id"));
+        Good good = goodRepository.findById(id).orElseThrow(() -> new ServiceException("There is no such good", HttpStatus.NOT_FOUND));
         return goodMapper.toDto(good);
     }
 
     public List<GoodDto> findAll(int size, int page) {
-        return goodMapper.toDto(goodRepository.findAll(size, page));
+        try {
+            return goodMapper.toDto(goodRepository.findAll(size, page));
+        } catch (RepositoryException ex) {
+            throw new ServiceException("Error during finding all goods", HttpStatus.BAD_REQUEST);
+        }
     }
 
     public GoodDto save(GoodCreationDto goodDto) {
-        Good entity = goodMapper.toEntity(goodDto);
-        return goodMapper.toDto(goodRepository.save(entity));
+        try {
+            Good entity = goodMapper.toEntity(goodDto);
+            return goodMapper.toDto(goodRepository.save(entity));
+        } catch (RepositoryException ex) {
+            throw new ServiceException("Error during saving good", HttpStatus.BAD_REQUEST);
+        }
     }
 
     public void update(Long id, GoodUpdateDto goodDto) {
-        goodRepository.update(id, goodMapper.toEntity(goodDto));
+        try {
+            goodRepository.update(id, goodMapper.toEntity(goodDto));
+        } catch (RepositoryException ex) {
+            throw new ServiceException("Error during updating good", HttpStatus.BAD_REQUEST);
+        }
     }
 
     public void delete(Long id) {
-        goodRepository.delete(id);
+        try {
+            goodRepository.delete(id);
+        } catch (RepositoryException ex) {
+            throw new ServiceException("Error during deleting good", HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
--- src/main/java/by/kladvirov/service/RoleService.java
+++ src/main/java/by/kladvirov/service/RoleService.java
@@ -2,11 +2,13 @@ package by.kladvirov.service;
 
 import by.kladvirov.dto.RoleCreationDto;
 import by.kladvirov.dto.RoleDto;
+import by.kladvirov.exception.RepositoryException;
 import by.kladvirov.exception.ServiceException;
 import by.kladvirov.mapper.RoleMapper;
 import by.kladvirov.model.Role;
 import by.kladvirov.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,25 +22,41 @@ public class RoleService {
     private final RoleMapper roleMapper;
 
     public RoleDto findById(Long id) {
-        Role role = roleRepository.findById(id).orElseThrow(() -> new ServiceException("There is no entity with the following id"));
+        Role role = roleRepository.findById(id).orElseThrow(() -> new ServiceException("There is no such role", HttpStatus.NOT_FOUND));
         return roleMapper.toDto(role);
     }
 
     public List<RoleDto> findAll(int size, int page) {
-        return roleMapper.toDto(roleRepository.findAll(size, page));
+        try {
+            return roleMapper.toDto(roleRepository.findAll(size, page));
+        } catch (RepositoryException ex) {
+            throw new ServiceException("Error during finding all roles", HttpStatus.BAD_REQUEST);
+        }
     }
 
     public RoleDto save(RoleCreationDto roleDto) {
-        Role entity = roleMapper.toEntity(roleDto);
-        return roleMapper.toDto(roleRepository.save(entity));
+        try {
+            Role entity = roleMapper.toEntity(roleDto);
+            return roleMapper.toDto(roleRepository.save(entity));
+        } catch (RepositoryException ex) {
+            throw new ServiceException("Error during saving role", HttpStatus.BAD_REQUEST);
+        }
     }
 
     public void update(Long id, RoleCreationDto roleDto) {
-        roleRepository.update(id, roleMapper.toEntity(roleDto));
+        try {
+            roleRepository.update(id, roleMapper.toEntity(roleDto));
+        } catch (RepositoryException ex) {
+            throw new ServiceException("Error during updating role", HttpStatus.BAD_REQUEST);
+        }
     }
 
     public void delete(Long id) {
-        roleRepository.delete(id);
+        try {
+            roleRepository.delete(id);
+        } catch (RepositoryException ex) {
+            throw new ServiceException("Error during deleting role", HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
