51e57c0e3b0f3cc7b4bcc73da869a0a3d14d0f96
Vlad Kirov <wladponomar@gmail.com>
2024-02-28 14:53:11 +0300

    feat: deleted

--- src/main/java/by/kladvirov/bpp/controller/ProfilingControllerMBean.java
+++
@@ -1,7 +0,0 @@
-package by.kladvirov.bpp.controller;
-
-public interface ProfilingControllerMBean {
-
-    void setEnabled(boolean isEnabled);
-
-}
--- src/main/java/by/kladvirov/exception/DataBaseException.java
+++
@@ -1,9 +0,0 @@
-package by.kladvirov.exception;
-
-public class DataBaseException extends RuntimeException {
-
-    public DataBaseException(String message) {
-        super(message);
-    }
-
-}
