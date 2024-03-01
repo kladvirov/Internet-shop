7343eb3363cb5c3236e66b9412fcf6072c209e91
Vlad Kirov <wladponomar@gmail.com>
2024-03-01 23:46:39 +0300

    feat: jpa repository

--- src/main/java/by/kladvirov/repository/UserRepository.java
+++ src/main/java/by/kladvirov/repository/UserRepository.java
@@ -1,20 +1,8 @@
 package by.kladvirov.repository;
 
 import by.kladvirov.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface UserRepository {
-
-    Optional<User> findById(Long id);
-
-    List<User> findAll(int size, int page);
-
-    User save(User user);
-
-    void update(Long id, User user);
-
-    void delete(Long id);
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
