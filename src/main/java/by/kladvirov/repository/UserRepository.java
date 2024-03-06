839a3e6fe385c836483accd39e21f93909d97377
Vlad Kirov <wladponomar@gmail.com>
2024-03-06 10:40:58 +0300

    feat: custom method added

--- src/main/java/by/kladvirov/repository/UserRepository.java
+++ src/main/java/by/kladvirov/repository/UserRepository.java
@@ -3,6 +3,10 @@ package by.kladvirov.repository;
 import by.kladvirov.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLogin(String login);
 
 }
