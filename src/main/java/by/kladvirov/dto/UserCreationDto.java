4c8ed30028b5a5dcfba378c46ef002efb9e2b621
Vlad Kirov <wladponomar@gmail.com>
2024-03-07 19:32:34 +0300

    feat: UserCreationDto since my authorization requires it

--- src/main/java/by/kladvirov/dto/UserCreationDto.java
+++ src/main/java/by/kladvirov/dto/UserCreationDto.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +16,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserCreationDto {
 
     @NotEmpty
