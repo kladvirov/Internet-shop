8df22312044085df0ea8d335e44a44dd50e8ae26
Vlad Kirov <wladponomar@gmail.com>
2024-03-09 00:53:11 +0300

    feat: roleIds to roles mapping

--- src/main/java/by/kladvirov/mapper/UserMapper.java
+++ src/main/java/by/kladvirov/mapper/UserMapper.java
@@ -2,22 +2,28 @@ package by.kladvirov.mapper;
 
 import by.kladvirov.dto.UserCreationDto;
 import by.kladvirov.dto.UserDto;
+import by.kladvirov.model.Role;
 import by.kladvirov.model.User;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "SPRING", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
+    @Mapping(target = "roles", source = "userCreationDto.roleIds", qualifiedByName = "mapRoleIdsToRoles")
     User toEntity(UserCreationDto userCreationDto);
 
     UserDto toDto(User user);
 
-    // need?
+    @Mapping(target = "roleIds", source = "user.roles", qualifiedByName = "mapRolesToRoleIds")
     UserCreationDto toCreationDto(User user);
 
     List<UserDto> toDto(List<User> users);
@@ -25,6 +31,24 @@ public interface UserMapper {
     @AfterMapping
     default void setBlockedToFalse(@MappingTarget User.UserBuilder user) {
         user.isBlocked(false);
+    }
+
+    @Named("mapRoleIdsToRoles")
+    default Set<Role> mapRoleIdsToRoles(List<Long> roleIds) {
+        return roleIds.stream()
+                .map(roleId -> {
+                    Role role = new Role();
+                    role.setId(roleId);
+                    return role;
+                })
+                .collect(Collectors.toSet());
+    }
+
+    @Named("mapRolesToRoleIds")
+    default List<Long> mapRolesToRoleIds(Set<Role> roles) {
+        return roles.stream()
+                .map(Role::getId)
+                .collect(Collectors.toList());
     }
 
 }
