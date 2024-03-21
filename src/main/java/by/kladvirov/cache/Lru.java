7bb12dc4bd860b978dd5843e10fff26ca4d340fc
Vlad Kirov <wladponomar@gmail.com>
2024-03-21 10:21:52 +0300

    feat: LRU impl

--- src/main/java/by/kladvirov/cache/Lru.java
+++ src/main/java/by/kladvirov/cache/Lru.java
@@ -10,33 +10,33 @@ public class Lru {
 
     private Integer CACHE_CAPACITY;
 
-    private List<Node> list = new LinkedList<>();
+    private List<Node> nodeList = new LinkedList<>();
 
     public Lru(Integer CACHE_CAPACITY){
         this.CACHE_CAPACITY = CACHE_CAPACITY;
     }
 
     public Object getElementFromCache(Object key) {
-        Node current = list.stream().filter(cache -> cache.getKey().equals(key)).findFirst().orElse(null);
+        Node current = nodeList.stream().filter(cache -> cache.getKey().equals(key)).findFirst().orElse(null);
         if(current != null){
-            list.remove(current);
-            list.addFirst(current);
+            nodeList.remove(current);
+            nodeList.addFirst(current);
             return current.getValue();
         }
         return null;
     }
 
     public void putElementInCache(Object key, Object value) {
-        Node current = list.stream().filter(cache -> cache.getKey().equals(key)).findFirst().orElse(null);
+        Node current = nodeList.stream().filter(cache -> cache.getKey().equals(key)).findFirst().orElse(null);
         if(current != null){
-            list.remove(current);
+            nodeList.remove(current);
         } else {
-            if(list.size() == CACHE_CAPACITY) {
-                list.removeLast();
+            if(nodeList.size() == CACHE_CAPACITY) {
+                nodeList.removeLast();
             }
         }
         Node newItem = new Node(key, value);
-        list.addFirst(newItem);
+        nodeList.addFirst(newItem);
     }
 
     @AllArgsConstructor
